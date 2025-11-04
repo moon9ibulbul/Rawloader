c29497f07dc3a48d898ca0b65d35660477aa8107
moon9ibulbul <62831103+moon9ibulbul@users.noreply.github.com>
2025-11-04 10:14:58 +0700

    Delete diag.php

--- diag.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-echo "disable_functions = ".ini_get('disable_functions')."\n";
-echo "exec: ".(function_exists('exec')?'ON':'OFF')."\n";
-echo "popen: ".(function_exists('popen')?'ON':'OFF')."\n";
-echo "proc_open: ".(function_exists('proc_open')?'ON':'OFF')."\n";
